d8396af253556ac0ecc8cd3fd3246ec65a6180ba
Hugo Valle <hugovalle1@.weber.edu>
2023-12-06 09:48:19 -0700

    Set initial code

--- city_weather.py
+++ city_weather.py
@@ -6,45 +6,15 @@ Class to model city weather
 class CityWeather:
     # TODO: Set constructor with the following attributes:
     # humidity, temp, temp_max, temp_min, city_name
-    def __init__(self, humidity, temp, temp_max, temp_min, city_name):
-        self._humidity = humidity
-        self._temp = temp
-        self._temp_max = temp_max
-        self._temp_min = temp_min
-        self._city_name = city_name
+    pass
+
     # TODO: Set all your "getters"
-    def get_humidity(self):
-        return self._humidity
-    
-    def get_temp(self):
-        return self._temp
-
-    def get_temp_max(self):
-        return self._temp_max
-
-    def get_temp_min(self):
-        return self._temp_min
-    
-    def get_city_name(self):
-        return self._city_name
 
     # TODO: Set your __gt__, __lt__, and __eq__ operators
     # based on the temp attribute
-    def __gt__(self, other):
-        return self._temp > other._temp
-
-    def __lt__(self, other):
-        return self._temp < other._temp
-
-    def __eq__(self, other):
-        return self._temp == other._temp
 
     # TODO: Set your __str__ operator which should display
     # all the attributes
-    def __str__(self):
-        return f'''{self._city_name} has a temperature of {self._temp}
-        with a humidity of {self._humidity}
-        a max temp of {self._temp_max} and a min temp of {self._temp_min}'''
 
 
 def main():
--- weather_app.py
+++ weather_app.py
@@ -3,50 +3,30 @@ Weather App to check the current weather based on zip code
 """
 from city_weather import CityWeather
 import requests
-from pprint import pprint as pp
+# from pprint import pprint as pp
 
 #Set your API Key
-API_KEY = 'af4f939501ebb649537e015196aedc70'
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  # TODO: Update with your API key
 # Base URL for OpenWeatherMap
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 
 def pull_weather_data(zip_code):
-    """Pull weather data from openweathermap.org
-    """
     api_url = BASE_URL + 'zip=' + str(zip_code) + ',us' \
         + '&units=metric' + '&appid=' + API_KEY 
-    # TODO: Add a try, except for this call
-    try:
-        # Connect to API
-        weather_data = requests.get(api_url).json()
-        # Response 200 is good
-        if weather_data['cod'] != 200:
-            raise ValueError
-    except ValueError:
-        print('Bad response from API')
-    return weather_data
+    # TODO: Call the API and store the response
+    # return the JSON response
+    pass
+
 
 def find_hottest_city(weather_zips):
-    """Find the hottest city in the list based on the temp attribute"""
-    hottest = None
-    for city in weather_zips:
-        if hottest is None:
-            hottest = city
-        elif city > hottest:
-            hottest = city
-    return hottest
+    # TODO: Find the hottest city
+    pass
 
 
 def find_coldest_city(weather_zips):
-    """Find the coldest city in the list based on the temp attribute"""
-    coldest = None
-    for city in weather_zips:
-        if coldest is None:
-            coldest = city
-        elif city < coldest:
-            coldest = city
-    return coldest
+    # TODO: Find the coldest city
+    pass
 
 
 def main():
@@ -54,32 +34,13 @@ def main():
     """
     #TODO: Read zipcodes from file
     zip_file = 'zip_codes.txt'
-    weather_zips = []
-    with open(zip_file, 'r') as data_file:
-        zip_codes = data_file.readlines()
-        for zip_code in zip_codes:
-            zip_code = zip_code.strip()
-            print(f'Pulling data for zip code: {zip_code}')
-            weather_data = pull_weather_data(zip_code)
-            city_name = weather_data['name']
-            print(f'Retreive information from:[{city_name}] city')
+            # TODO: Call pull_weather_data for each zip code
+            
             # TODO: Store data in a CityWeather object
-            city_weather = CityWeather(weather_data['main']['humidity'],
-                            weather_data['main']['temp'],
-                            weather_data['main']['temp_max'],
-                            weather_data['main']['temp_min'],
-                            weather_data['name'])
-            weather_zips.append(city_weather)
     # TODO: Compare the current temperture of all objects
     # TODO: print the hotest and coldest
-    hottest = find_hottest_city(weather_zips)
-    print(f'The hottest city is {hottest.get_city_name()} with a current conditions {hottest}')
-    coldest = find_coldest_city(weather_zips)
-    print(f'The coldest city is {coldest.get_city_name()} with a current conditions {coldest}')
 
     
 
 if __name__ == '__main__':
     main()
-
-# Connect to openweathermap.org api to read the 16 day forcast 
